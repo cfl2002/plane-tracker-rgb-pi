31d2ab793556231246e841341b6b8d0ecdb641a9
cfl2002 <81695613+cfl2002@users.noreply.github.com>
2025-07-12 20:27:14 -0500

    Update config.py

--- its-a-plane-python/config.py
+++ its-a-plane-python/config.py
@@ -1,19 +1,19 @@
 ZONE_HOME = {
-    "tl_y": xx.xxxxxx, # Top-Left Latitude (deg) https://www.latlong.net/ the bigger the zone, the more planes you'll get
-    "tl_x": xx.xxxxxx, # Top-Left Longitude (deg)
-    "br_y": xx.xxxxxx, # Bottom-Right Latitude (deg)
-    "br_x": xx.xxxxxx # Bottom-Right Longitude (deg)
+    "tl_y": 44.985928, # Top-Left Latitude (deg) https://www.latlong.net/ the bigger the zone, the more planes you'll get
+    "tl_x": -93.312035, # Top-Left Longitude (deg)
+    "br_y": 44.960668, # Bottom-Right Latitude (deg)
+    "br_x": -93.242340, # Bottom-Right Longitude (deg)
 }
 LOCATION_HOME = [
-    xx.xxxxxx, # Latitude (deg)
-    xx.xxxxxx # Longitude (deg)
+    44.971718, # Latitude (deg)
+    -93.278106, # Longitude (deg)
 ]
 TEMPERATURE_LOCATION = "xx.xxxxxx,xx.xxxxxx" #same as location home
 TOMORROW_API_KEY = "xxxxxxx" # Get an API key from https://tomorrow.io they only allows 25 pulls an hour, if you reach the limit you'll need to wait until the next hour 
 TEMPERATURE_UNITS = "imperial" #can use "metric" if you want, same for distance 
 DISTANCE_UNITS = "imperial"
 CLOCK_FORMAT = "12hr" #use 12hr or 24hr
-MIN_ALTITUDE = 2600 #feet
+MIN_ALTITUDE = 1500 #feet
 BRIGHTNESS = 100
 BRIGHTNESS_NIGHT = 50
 NIGHT_BRIGHTNESS = False #True for on False for off
